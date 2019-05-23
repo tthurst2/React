62c50c4e5962cd26b5e5c49889659240bd0602f7
Thurston <tylerthurston1@gmail.com>
2019-05-23 16:29:14 -0400

    added bootstrap button and badge styling

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,14 +1,21 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class Counter extends Component {
+  state = {
+    count: 0
+  };
   render() {
-    React.createElement("h1");
     return (
       <div>
-        <h1>Hello World</h1>
-        <button>Increment</button>
+        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? 'Zero' : count;
   }
 }
 

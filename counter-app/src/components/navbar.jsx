0979c3e467bd970f23004e75f1228806d68a43b6
Thurston <tylerthurston1@gmail.com>
2019-05-30 15:46:04 -0400

    converted class to stateless functional component

--- counter-app/src/components/navbar.jsx
+++ counter-app/src/components/navbar.jsx
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 
+// Stateless Functional Component
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar{' '}
+        <span className="badge badge-pill badge-secondary">
+          {this.props.totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
 class Navbar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar{' '}
-          <span className="badge badge-pill badge-secondary">
-            {this.props.totalCounters}
-          </span>
-        </a>
-      </nav>
-    );
-  }
+  render() {}
 }
 
 export default Navbar;

0a09c8cc0032814a74f997bd2c2233a9c8d2213f
Thurston <tylerthurston1@gmail.com>
2019-05-30 14:35:09 -0400

    added handleReset function

--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -10,7 +10,13 @@ class Counters extends Component {
       { id: 4, value: 0 }
     ]
   };
-
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
   handleDelete = counterId => {
     const counters = this.state.counters.filter(c => c.id !== counterId);
     this.setState({ counters });
@@ -20,7 +26,12 @@ class Counters extends Component {
   render() {
     return (
       <div>
-        <button className="btn btn-primary btn-sm m-2">Reset</button>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
         {this.state.counters.map(counter => (
           <Counter
             key={counter.id}

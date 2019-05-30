916b2b66aab66ee18c1ace6f2829f40b0f848cda
Thurston <tylerthurston1@gmail.com>
2019-05-30 15:47:20 -0400

    added props as parameter

--- counter-app/src/components/navbar.jsx
+++ counter-app/src/components/navbar.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
 // Stateless Functional Component
-const NavBar = () => {
+const NavBar = props => {
   return (
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="#">
         Navbar{' '}
         <span className="badge badge-pill badge-secondary">
-          {this.props.totalCounters}
+          {props.totalCounters}
         </span>
       </a>
     </nav>

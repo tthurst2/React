974205cf3428ad7e7406398eba22a3b6a75de662
Thurston <tylerthurston1@gmail.com>
2019-05-30 16:11:11 -0400

    changed to href value to refresh page and avoid warning

--- counter-app/src/components/navbar.jsx
+++ counter-app/src/components/navbar.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
 // Stateless Functional Component
-const NavBar = ({ props }) => {
+const NavBar = props => {
   return (
     <nav className="navbar navbar-light bg-light">
-      <a className="navbar-brand">
+      <a className="navbar-brand" href="./">
         Navbar{' '}
         <span className="badge badge-pill badge-secondary">
           {props.totalCounters}

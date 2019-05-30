336901e0977566323dc292cbbca5f8ae240586a4
Thurston <tylerthurston1@gmail.com>
2019-05-30 16:04:32 -0400

    added constructor

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -13,6 +13,15 @@ class App extends Component {
     ]
   };
 
+  constructor() {
+    super();
+    console.log('App - Constructor');
+  }
+
+  componentDidMount() {
+    console.log('App - Mounted');
+  }
+
   handleIncrement = counter => {
     const counters = [...this.state.counters];
     const index = counters.indexOf(counter);
@@ -28,6 +37,7 @@ class App extends Component {
     });
     this.setState({ counters });
   };
+
   handleDelete = counterId => {
     const counters = this.state.counters.filter(c => c.id !== counterId);
     this.setState({ counters });
@@ -35,6 +45,8 @@ class App extends Component {
   };
 
   render() {
+    console.log('App - Rendered');
+
     return (
       <React.Fragment>
         <NavBar
